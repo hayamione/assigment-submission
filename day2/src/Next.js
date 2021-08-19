dd1f1c1c1d2063400f1986a70c3e4c61ab0657fd
hayamione <62325910+hayamione@users.noreply.github.com>
2021-08-19 22:31:45 +0530

    Day 2 Assignment

---
+++ day2/src/Next.js
@@ -0,0 +1,9 @@
+function Next (props) {
+    return (
+        <div>
+            <h2>{props.name}</h2>
+        </div>
+    )
+}
+
+export default Next;
---
+++ day2/src/Service.js
@@ -0,0 +1,18 @@
+function Service(props){
+
+return(
+    <div className="card">
+        <div className="card-img">
+            <img className="card-image" src={props.image} alt={props.image}/>
+        </div>
+        <h2 className="card-title">{props.title}</h2>
+        <p className="card-desc">{props.desc}</p>
+        <p className="btn-parent">
+            <button className="card-btn">Read More</button>
+        </p>
+    </div>
+)
+
+}
+
+export default Service;
