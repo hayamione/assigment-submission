7e94b7c565a34eee3c913022acfecb9651f85982
Haya Zubair <62325910+hayamione@users.noreply.github.com>
2021-08-23 13:40:45 +0530

    Add files via upload

---
+++ day3/src/App.js
@@ -0,0 +1,114 @@
+import './App.css';
+import {useState,useEffect} from 'react';
+
+
+function App() {
+
+  
+let [movieinfo,setMovieinfo]=useState(null);
+let [title,setTitle]=useState("harry potter and the order of the phoenix");
+
+useEffect(()=>{
+
+  getmoviedata(title);  
+  
+},[])
+
+
+function readTitle(value){
+    setTitle(value);
+}
+
+function getmoviedata(){
+  let url = `https://omdbapi.com/?t=${title}&apikey=4926feba`;
+
+  fetch(url)
+  .then((response)=>response.json())
+  .then((movie)=>{
+    console.log(movie);
+    setMovieinfo(movie);
+})
+.catch((err)=>{
+  console.log(err);
+})
+}
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="padd">
+          <h1>Movie Search App With React JS</h1>
+          <div className="search">
+            <input type="text" placeholder="Enter Movie Name" onChange={(event)=>{readTitle(event.target.value)}} className="inp"></input>
+            <button Onclick={getmoviedata} className="btn">Search</button>
+          </div>
+
+        {
+          movieinfo?.Error===undefined?(
+        
+          <div class="movie">
+            
+              <div class="poster">
+                <img src={movieinfo?.Poster} alt="Poster" className="img" />
+              </div>
+              <div class="details">
+                <div className="padd">
+                  <h2>{movieinfo?.Title}</h2>
+                  <p><strong>Genre :</strong> {movieinfo?.Genre}</p>
+                  <p><strong>Plot :</strong> {movieinfo?.Plot}</p>
+                  <p><strong>Actors :</strong> {movieinfo?.Actors}</p>
+                  <p><strong>Director :</strong> {movieinfo?.Director}</p>
+                  <p><strong>Writer :</strong> {movieinfo?.Writer}</p>
+                  <p><strong>Box Office :</strong> {movieinfo?.BoxOffice}</p>
+                  <p><strong>Release Date :</strong> {movieinfo?.Released}</p>
+                  <p><strong>Runtime :</strong> {movieinfo?.Runtime}</p>
+                  <p><strong>Language :</strong> {movieinfo?.Language}</p>
+                  <p><strong>Country :</strong> {movieinfo?.Country}</p>
+                  <p><strong>Awards :</strong> {movieinfo?.Awards}</p>
+                  <p><strong>Production :</strong> {movieinfo?.Production}</p>
+                  <p><strong>Ratings</strong></p>
+
+                  <div className="rating">
+                    {   
+                  movieinfo?.Ratings.map((rating,index)=>(
+
+                    <div key={index}>
+                        <strong>{rating.Source}</strong>
+                        <p>{rating.Value}</p>
+                    </div>
+                  ))
+                    }
+                  </div>
+
+                </div>
+              </div>
+          </div>
+          
+          ):
+
+          (
+            <h1>Movie Not Found</h1>
+          )   
+
+      }
+        </div>
+      </div>
+      {/*<header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
