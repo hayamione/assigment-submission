2cf92967fa950f56bab5587f009a6d0fa270fa54
Haya Zubair <62325910+hayamione@users.noreply.github.com>
2021-08-27 23:21:07 +0530

    JS Essentials Day 2 Assignment Completed

---
+++ js-essentials-day2/script.js
@@ -0,0 +1,25 @@
+// loops 
+// while 
+// do-while 
+// for 
+
+let food = [{
+        name: "Burger",
+        price: 89,
+        quantity: 5
+    },
+    {
+        name: "Pasta",
+        price: 149,
+        quantity: 2
+    },
+    {
+        name: "Pizza",
+        price: 400,
+        quantity: 6
+    }
+]
+
+for (let i = 0; i < food.length; i++) {
+    console.log(food[i]);
+}
