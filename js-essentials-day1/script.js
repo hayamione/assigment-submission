2ba3f1a23480986717c8f1790f93de7ae49dd012
Haya Zubair <62325910+hayamione@users.noreply.github.com>
2021-08-27 23:02:51 +0530

    JS-Essentials-day-1 Assignment Submitted

---
+++ js-essentials-day1/script.js
@@ -0,0 +1,6 @@
+a = 800
+b = 40
+document.getElementById("id1").innerHTML = (a + b)
+document.getElementById("id2").innerHTML = (a - b)
+document.getElementById("id3").innerHTML = (a * b)
+document.getElementById("id4").innerHTML = (a / b)
